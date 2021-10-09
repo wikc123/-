2d50f368f56c406fb31ac3c7ad23dfb78bf13f5d
wikc123 <92205809+wikc123@users.noreply.github.com>
2021-10-09 16:52:51 +0800

    Add files via upload

---
+++ 台阶递推.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int step(int n)
+{
+ size_t n1 = 0, n2 = 1;
+ for (size_t i = 0; i < n; i++)
+ {
+  size_t n3 = n1;
+  n1 = n2;
+  n2 += n3;
+ }
+ return n2;
+}
+int main() {
+ int n;
+ cin >> n;
+ cout << step(n) << endl;
+ return 0;
+ }
